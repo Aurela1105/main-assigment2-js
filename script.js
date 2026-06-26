3884e0cc10912a3253066593fb5d6a6acd4741b3
ajajff10-hub <ajaj.ff10@gmail.com>
2026-06-26 22:03:55 +0300

    playRound function

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function computerPlay() {
 
     return choices[randomIndex];
 }
+function playRound (playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return `It's a tie! Both chose ${playerSelection}`;
+    }
+    if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') || 
+        (playerChoice === 'paper' && computerChoice === 'rock') || 
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
